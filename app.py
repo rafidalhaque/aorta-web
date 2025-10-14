3f1bbd029142fd5114eb2445bf9968091e717a15
rafidalhaque <rafidalhaque@proton.me>
2025-10-14 18:28:20 +0600

    feat: homepage's hero section

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index_page():  # put application's code here
     return render_template("index.html"), 400
